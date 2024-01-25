4722f50a6ebbb3677dcceb8479980484a6526ea2
JaviPavon <javierpavonborrero@gmail.com>
2024-01-25 13:06:24 +0100

    Creación de los primeros modelos

--- mundialitopx/admin.py
+++ mundialitopx/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Usuario, Pais, Piloto, Escuderia, Carrera, Circuito
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+
+admin.site.register(Usuario,UserAdmin)
+admin.site.register(Pais)
+admin.site.register(Piloto)
+admin.site.register(Escuderia)
+admin.site.register(Carrera)
+admin.site.register(Circuito)
--- mundialitopx/models.py
+++ mundialitopx/models.py
@@ -1,3 +1,72 @@
+import datetime
+from wsgiref.validate import validator
+from django.conf import settings
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import MaxValueValidator,MinValueValidator
 
-# Create your models here.
+class Usuario(AbstractUser):
+
+    def __str__(self):
+        return self.username
+
+
+class Pais(models.Model):
+    nombre = models.CharField(max_length=100)
+    bandera = models.ImageField(upload_to='banderas/', null=True, blank=True)
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        verbose_name_plural = 'Paises'
+
+
+class Circuito(models.Model):
+    nombre = models.CharField(max_length=100)
+    alias = models.CharField(max_length=100)
+    pista = models.ImageField(upload_to='circuitos/', null=True, blank=True)
+    pais = models.ForeignKey(Pais, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nombre
+
+class Escuderia(models.Model):
+    nombre = models.CharField(max_length=100)
+    alias = models.CharField(max_length=100)
+    monoplaza = models.ImageField(upload_to='monoplazas/', null=True, blank=True)
+    pais = models.ForeignKey(Pais, on_delete=models.CASCADE)
+    logo = models.ImageField(upload_to='logos/', null=True, blank=True)
+    puesto = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(20)])
+    descripcion = models.TextField()
+    puntos = models.IntegerField()
+
+    def __str__(self):
+        return self.nombre
+    
+class Piloto(models.Model):
+    nombre = models.CharField(max_length=100)
+    puntos = models.IntegerField()
+    pais = models.ForeignKey(Pais, on_delete=models.CASCADE)
+    dorsal = models.IntegerField()
+    escuderia = models.ForeignKey(Escuderia, on_delete=models.CASCADE)
+    puesto = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(20)])
+    biografia = models.TextField()
+    foto = models.ImageField(upload_to='fotospilotos/', null=True, blank=True)
+
+    def __str__(self):
+        return self.nombre
+
+
+class Carrera(models.Model):
+    circuito = models.ForeignKey(Circuito, on_delete=models.CASCADE)
+    puesto = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(20)])
+    piloto = models.ForeignKey(Piloto, on_delete=models.CASCADE)
+    puntos = models.IntegerField()
+
+    def __str__(self):
+        return str(self.piloto)+ " en " + str(self.circuito)
+
+    class Meta:
+        unique_together= ['circuito','piloto']
