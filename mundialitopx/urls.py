b782fbafa570eb8934e0073d33c97f08eeff63c8
javipavon <javierpavonborrero@gmail.com>
2024-01-26 22:37:48 +0100

    Urls de las nuevas vistas

--- mundialitopx/urls.py
+++ mundialitopx/urls.py
@@ -2,8 +2,30 @@ from django.urls import path
 from . import views
 from django.contrib.admin.views.decorators import staff_member_required
 from django.contrib.auth.decorators import login_required
-# from .views import (
-# )
+from .views import (
+    DetallesPais, 
+    CrearPais, 
+    EditarPais, 
+    BorrarPais, 
+
+    DetallesPiloto, 
+    CrearPiloto, 
+    EditarPiloto, 
+    BorrarPiloto, 
+
+    DetallesEscuderia,
+    CrearEscuderia, 
+    EditarEscuderia, 
+    BorrarEscuderia, 
+
+    DetallesCircuito, 
+    CrearCircuito, 
+    EditarCircuito, 
+    BorrarCircuito, 
+
+    DetallesCarrera, 
+    BorrarCarrera, 
+)
 
 urlpatterns = [
     path('', staff_member_required(views.admin), name='admin'),
@@ -12,4 +34,28 @@ urlpatterns = [
     path('inicio/admin/ecuderia', staff_member_required(views.escuderiaAdmin), name='escuderia'),
     path('inicio/admin/circuito', staff_member_required(views.circuitoAdmin), name='circuito'),
     path('inicio/admin/carrera', staff_member_required(views.carreraAdmin), name='carrera'),
+
+    path('inicio/admin/pais/detalle/<int:pk>', DetallesPais.as_view(), name='detalle_pais'),
+    path('inicio/admin/pais/borrar/<int:pk>', BorrarPais.as_view(), name='borrar_pais'),
+    path('inicio/admin/pais/crear', CrearPais.as_view(), name='crear_pais'),
+    path('inicio/admin/pais/editar/<int:pk>', EditarPais.as_view(), name='editar_pais'),
+
+    path('inicio/admin/ecuderia/detalle/<int:pk>', DetallesEscuderia.as_view(), name='detalle_ecuderia'),
+    path('inicio/admin/ecuderia/borrar/<int:pk>', BorrarEscuderia.as_view(), name='borrar_ecuderia'),
+    path('inicio/admin/ecuderia/crear', CrearEscuderia.as_view(), name='crear_ecuderia'),
+    path('inicio/admin/ecuderia/editar/<int:pk>', EditarEscuderia.as_view(), name='editar_ecuderia'),
+
+    path('inicio/admin/piloto/detalle/<int:pk>', DetallesPiloto.as_view(), name='detalle_piloto'),
+    path('inicio/admin/piloto/borrar/<int:pk>', BorrarPiloto.as_view(), name='borrar_piloto'),
+    path('inicio/admin/piloto/crear', CrearPiloto.as_view(), name='crear_piloto'),
+    path('inicio/admin/piloto/editar/<int:pk>', EditarPiloto.as_view(), name='editar_piloto'),
+
+    path('inicio/admin/circuito/detalle/<int:pk>', DetallesCircuito.as_view(), name='detalle_circuito'),
+    path('inicio/admin/circuito/borrar/<int:pk>', BorrarCircuito.as_view(), name='borrar_circuito'),
+    path('inicio/admin/circuito/crear', CrearCircuito.as_view(), name='crear_circuito'),
+    path('inicio/admin/circuito/editar/<int:pk>', EditarCircuito.as_view(), name='editar_circuito'),
+
+    path('inicio/admin/carrera/detalle/<int:pk>', DetallesCarrera.as_view(), name='detalle_carrera'),
+    path('inicio/admin/carrera/borrar/<int:pk>', BorrarCarrera.as_view(), name='borrar_carrera'),
+
 ]
