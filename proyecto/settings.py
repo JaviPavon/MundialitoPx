1eabefdc11d9b17f67ccdca6b28301b04f1b55c1
javipavon <javierpavonborrero@gmail.com>
2024-04-06 20:19:12 +0200

    Añadido archivo de configuración settings.py

--- proyecto/settings.py
+++ proyecto/settings.py
@@ -140,7 +140,10 @@ if not DEBUG:
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
-STATIC_ROOT = BASE_DIR / "static"
+STATIC_ROOT = [
+    BASE_DIR / "static",
+    '/var/www/static/',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
