10cb0f41b8bdff4d3e15168bc66f3f48bd4e35b9
Javier Pavón Borrero <146813030+JaviPavon@users.noreply.github.com>
2025-08-27 13:23:27 +0200

    Delete build-app.sh

--- build-app.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-npm run build || true   # quítalo si no usas frontend con Vite/React
-php artisan optimize:clear
-php artisan config:cache
-php artisan event:cache
-php artisan route:cache
-php artisan view:cache
